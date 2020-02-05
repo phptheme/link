6820e500860581d88ad4a6eeea09488b13263b3f
phptheme <dev@getphptheme.com>
2020-02-05 17:47:33 +0300

    n/a

--- Link.php
+++ Link.php
@@ -6,6 +6,42 @@
  */
 namespace PhpTheme\Link;
 
-class Link extends BaseLink
+class Link extends \PhpTheme\Tag\Tag
 {
+
+    public $tag = 'a';
+
+    public $label;
+
+    public $url;
+
+    public $icon;
+
+    public $iconTemplate = '<i class="{icon}"></i>{label}';
+
+    public function getContent()
+    {
+        $return = $this->label;
+
+        if ($this->icon)
+        {
+            $return = strtr($this->iconTemplate, [
+                '{label}' => $this->label,
+                '{icon}' => $this->icon
+            ]);
+        }
+
+        return $return;
+    }
+
+    public function toString() : string
+    {
+        if (($this->url) && ($this->tag == 'a'))
+        {
+            $this->attributes['href'] = $this->url;
+        }
+
+        return parent::toString();
+    }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# link
+# HTML Link
